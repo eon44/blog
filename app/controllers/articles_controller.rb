c989ff97308e85a4cf14b3c3a876c74427fdb6ce
Robert Kamiński <rkaminski83@gmail.com>
2014-10-09 22:50:38 +0200

    Następna wersja

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,16 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
- 
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
